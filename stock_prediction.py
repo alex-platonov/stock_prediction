cc697074840e0537ee3ea076eefe4dd414842abf
alex-platonov <154932143+alex-platonov@users.noreply.github.com>
2023-12-30 11:36:43 +0100

    Update stock_prediction.py

--- stock_prediction.py
+++ stock_prediction.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 import scikit_learn as sklearn
+
 model = load_model('Stock_Predictions_Model.keras')
 
 st.header('Stock Market Predictor')
